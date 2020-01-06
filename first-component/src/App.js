5a5b97f99dab2f173f1563212895e84d4b2d5412
Shawinder706 <shawinder771@gmail.com>
2020-01-06 13:58:44 +0530

    Add onchange In jsx

--- first-component/src/App.js
+++ first-component/src/App.js
@@ -21,16 +21,33 @@ class App extends Component{
         ]
       })
   }
+
+  switchNameHandler=(event)=>{
+    this.setState({
+      
+        person:[{
+          name:"Aman",age:21},
+          {name:event.target.value,age:22},
+        ]
+      
+    })
+  }
   
 
   render(){
     return (
       <div className="App">
         
-        <Person name={this.state.person[0].name} age={this.state.person[0].age}></Person>
-        <Person name={this.state.person[1].name} age={this.state.person[1].age}></Person>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age} click= {this.nameChangeHandler.bind(this,'method refernce')}></Person>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age} change={this.switchNameHandler}></Person>
         {/* <button onClick={() => {return this.nameChangeHandler("myname")} }>click me</button> */}
+        {/* <button onClick={this.nameChangeHandler.bind(this,'max') } >click me</button> */}
         <button onClick={this.nameChangeHandler.bind(this,'max') } >click me</button>
+
+        {/* This way is not working beacause when js render in browser function executes automatically
+          <button onClick={ this.nameChangeHandler("myname") }>click me</button> 
+         */}
+        
         
       </div>
     );
