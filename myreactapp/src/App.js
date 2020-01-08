eee2e72bb317c4bdc34543382a2511e7cc66d69e
Shawinder706 <shawinder771@gmail.com>
2020-01-08 15:13:16 +0530

    key and list practise

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () =>
+{
+  const items = [
+    { name: 'Bread', price: 5 },
+    { name: 'Milk', price: 15 },
+    { name: 'Eggs', price: 25 },
+    { name: 'Tea', price: 55 },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <List items={items}></List>
+      <Employee name="Aman" age={21} position="Dev" />
+      <GroceryList item1="Apples" item2="Oranges" item3="Mangoes" />
     </div>
-  );
+  )
+}
+
+
+const List = (props) =>
+{
+  const totalItems = props.items.map((curItem, index) =>
+  {
+    return <li key={index}>{curItem.name} and price is {curItem.price}</li>
+  })
+  return (
+    <ul>{totalItems}</ul>
+  )
+}
+
+const Employee = (props) =>
+{
+  return (
+    <div>
+      <p>My name is {props.name},{props.age} years old and working as a {props.position} </p>
+    </div>
+  )
+}
+
+const GroceryList = (props) =>
+{
+
+  return (
+    <ul>
+      <li>{props.item1}</li>
+      <li>{props.item2}</li>
+      <li>{props.item3}</li>
+    </ul>
+  )
+
 }
 
 export default App;
